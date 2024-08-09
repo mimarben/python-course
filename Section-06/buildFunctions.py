48ff5fe47f90bdcd1f6c5d565e2b64c07c13374c
martinm <mimarben@gmail.com>
2024-08-09 11:22:53 +0200

    Section 7

---
+++ Section-06/buildFunctions.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/3/library/functions.html
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def my_function():
+  print("Hello, World!")
+
+my_function()
