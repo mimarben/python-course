1e0cd12a1141240501992c4713d2ad04bb498e73
miguel <mimarben@gmail.com>
2024-10-11 09:44:37 +0200

    start turtle race

--- Section-19/turtle-race-start/main.py
+++ Section-19/turtle-race-start/main.py
@@ -2,6 +2,9 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
+screen.setup(width=800, height=800)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color:")
+
 
 
 
