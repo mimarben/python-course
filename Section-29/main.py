6acfc150f3b449a6e5f451c86768af21ba777456
mimarben <mimarben@gmail.com>
2024-11-01 17:07:14 +0100

    Entry

--- Section-29/main.py
+++ Section-29/main.py
@@ -16,17 +16,20 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=2, row=0)
 
-website_lable = Label(text="WebSite",font=(FONT_NAME, 20, "bold"))
+website_lable = Label(text="WebSite",font=(FONT_NAME, 12, "bold"))
 website_lable.grid(column=0, row=1)
-website_text=canvas.create_text(100, 140, text="", fill="white", font=(FONT_NAME, 35, "bold"))
-website_text.grid(column=1, row=1)
+website_text=Entry(width=35)
+website_text.grid(row=1, column=1, columnspan=2)
 
-email_label = Label(text="Email", font=(FONT_NAME, 20, "bold"))
+email_label = Label(text="Email", font=(FONT_NAME, 12, "bold"))
 email_label.grid(column=0, row=2)
+email_text=Entry(width=35)
+email_text.grid(row=2, column=1, columnspan=2)
 
-password_label = Label(text="Password", font=(FONT_NAME, 20, "bold"))
+password_label = Label(text="Password", font=(FONT_NAME, 12, "bold"))
 password_label.grid(column=0, row=3)
-
+password_text=Entry(width=35)
+password_text.grid(row=3, column=1, columnspan=2)
 
 #canvas.pack()
 window.mainloop()
