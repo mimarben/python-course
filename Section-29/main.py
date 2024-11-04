c98f3de78670b3e840a8850dd4a80cf17b44cb36
mimarben <mimarben@gmail.com>
2024-11-04 18:09:45 +0100

    section-30 end

--- Section-29/main.py
+++ Section-29/main.py
@@ -1,14 +1,48 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+
 FONT_NAME = "Courier"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
 def generate_password():
-    pass
+    #Password Generator Project
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = []
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_letters = password_letters + [random.choice(symbols) for _ in range(nr_symbols)]
+    password_letters = password_letters + [random.choice(numbers) for _ in range(nr_numbers)]
+    
+    random.shuffle(password_letters)
+    
+    password= "".join(password_letters)
+    password_text.insert(0,password)
+    print(f"Your password is: {password}")
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    pass
+    new_line = f"{website_text.get()} | {email_text.get()} | {password_text.get()}\n"
+    if len(password_text.get()) > 0 and len(website_text.get()) > 0 and len(email_text.get()) > 0:
+        confirmation=messagebox.askokcancel(title="Save Password", message=f"These are the details for:\n Website: {website_text.get()}\nEmail: {email_text.get()}\nPassword: {password_text.get()}")
+        if confirmation==True:
+            with open("password.txt", "a") as file:
+                file.write(new_line)    
+            website_text.delete(0, END)
+            password_text.delete(0, END)
+            email_text.delete(0, END)
+    else:
+        messagebox.showinfo(title="Oops", message="Please make sure to fill out all fields.")
+# ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Generator")
 window.config(padx=50, pady=50)
@@ -31,8 +65,10 @@ password_label.grid(column=0, row=3)
 #Entries
 website_text=Entry(width=45)
 website_text.grid(row=1, column=1, columnspan=2)
+website_text.focus()
 email_text=Entry(width=45)
 email_text.grid(row=2, column=1, columnspan=2)
+email_text.insert(0, "example@email.com")
 password_text=Entry(width=21)
 password_text.grid(row=3, column=1)
 generate_button = Button(text="Generate Password", font=(FONT_NAME, 12), command=generate_password, width=18)
