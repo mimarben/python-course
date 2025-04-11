224ceaacccb7a4286162afbc7e3f9dca931b093b
mimarben <mimaben@gmail.com>
2025-04-11 12:54:38 +0200

    Section 71

--- Section-68/main.py
+++ Section-68/main.py
@@ -43,7 +43,7 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html", logged_in=current_user.is_authenticated)
+    return render_template("index.html", logged_in=True)
 
 
 @app.route('/register', methods=["GET", "POST"])
